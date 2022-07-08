9976b7483a80b491a974fe03cde1778f4216da40
supriya855 <83066985+supriya855@users.noreply.github.com>
2022-07-08 19:29:42 +0530

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,8 @@
+package com.supriya;
+import java.io.IOException;
+public class ATM extends OptionMenu {
+        public static void main(String[] args) throws IOException {
+            OptionMenu optionMenu = new OptionMenu();
+            optionMenu.getLogin();
+        }
+    }
